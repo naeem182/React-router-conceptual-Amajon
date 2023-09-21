0b69d8446781737bbb5b22deca656b957f79bfbc
naeem <naeemislamjr182@gmail.com>
2023-09-22 00:22:44 +0600

    discuss about path route

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const myCreatedRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>This is my first router </div>
+  },
+  {
+    path: "/products",
+    element: <div>products router</div>
+  },
+  {
+    path: "/about",
+    element: <div>about router</div>
+  },
+
+])
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={myCreatedRouter}></RouterProvider>
   </React.StrictMode>,
 )
